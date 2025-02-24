25b9ff7b2df0f748121e0446367b0bb95bffde5f
Wes <millswa2@appstate.edu>
2025-02-24 14:26:18 -0500

    Added ticker name and company name to the print based on input.

--- app.py
+++ app.py
@@ -3,17 +3,24 @@ import yfinance as yf
 
 app = Flask(__name__)
 
+# To run the program, open the terminal and enter: python app.py runserver
+#   YFRateLimitError? Try updating yfinance: pip install --upgrade yfinance
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     price = None
+    error_message = None
+    longName = None
     if request.method == "POST":
         ticker_name = request.form.get("stockTicker")
         if ticker_name:
             ticker = yf.Ticker(ticker_name)
             info = ticker.info
+            longName = info['longName']
             price = info.get("regularMarketPrice", "Price not found")
-    return render_template("firstsite.html", value=price)
+
+    return render_template("firstsite.html", ticker_name=ticker_name, value=price, longName=longName, error_message=error_message)
 
 
 if __name__ == "__main__":
