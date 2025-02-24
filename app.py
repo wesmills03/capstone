7cf386d4583d7d4a417906bf3fcab242b9f7d8f4
Wes <millswa2@appstate.edu>
2025-02-24 16:02:47 -0500

    Created equation for annual dividend growth rate. First steps for calculating DDM

--- app.py
+++ app.py
@@ -7,19 +7,42 @@ app = Flask(__name__)
 #   YFRateLimitError? Try updating yfinance: pip install --upgrade yfinance
 
 
+# Returns annual dividend growth rate for each year
+def get_dividend_g(stock):
+    dividends = stock.dividends  # historical dividends
+
+    if dividends.empty:
+        return None, None
+
+    annual_dividend = dividends.resample('Y').sum()  # total annual dividend
+    annual_dividend_growth_rate = annual_dividend.pct_change().dropna()  # dividend growth rate for each year 
+
+    return annual_dividend_growth_rate
+
+    # geometric_mean_dividend_growth_rate = (annual_dividend_growth_rate + 1).prod() ** (1/len(annual_dividend_growth_rate)) - 1  # geometric mean to account for compounding data
+    # arithmetic_mean_dividend_growth_rate = np.mean(annual_dividend_growth_rate)  # arithmetic mean for posterity
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     price = None
     error_message = None
     longName = None
     ticker_name = None
+    # fair_value = None
+
     if request.method == "POST":
         ticker_name = request.form.get("stockTicker")
+
         if ticker_name:
+            # Fetch stock data
             ticker = yf.Ticker(ticker_name)
             info = ticker.info
-            longName = info['longName']
+
+            longName = info["longName"]
             price = info.get("regularMarketPrice", "Price not found")
+
+            # D = info.get("dividendRate", 0) # Annual Dividend (D)
 
     return render_template("firstsite.html", ticker_name=ticker_name, value=price, longName=longName, error_message=error_message)
 
